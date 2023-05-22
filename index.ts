32f4ad9d6c7a9f8ca348f311bea205154fc1597b
anisbastide <anis.bastide@odigo.com>
2023-05-23 00:13:11 +0200

    init express server and add jwt token

--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken'
 
 dotenv.config();
 
-const app = express();
+const app: Express = express();
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json?.()
 const port = process.env.PORT;
+const secretToken = process.env.SECRET ? process.env.SECRET : ''
 
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Bonjour');
 });
+app.post('/api/login',jsonParser,(req: Request<{email:string}>, res: Response) => {
+    console.log(req.body)
+    const email = req.body.email
+    const token = jwt.sign({email},secretToken)
+    res.send(token);
+})
 
+app.post('/api/justify',jsonParser,(req: Request<{token:string,text:string}>, res: Response) => {
+   const decoded = jwt.verify(req.body.token,secretToken)
+    if(decoded){
+        res.send(req.body.text);
+    }
+})
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
