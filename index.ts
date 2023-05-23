bada0f074c67e43e78b02b50a34c2bc8c62d3c08
anisbastide <anis.bastide@odigo.com>
2023-05-23 23:22:07 +0200

    make justify function work + fix tests + add limit

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json?.()
 const port = process.env.PORT;
 const secretToken = process.env.SECRET ? process.env.SECRET : ''
+let maxWordsPerDay = 8000;
+let currentDay = new Date();
 
 
 app.get('/', (req: Request, res: Response) => {
@@ -17,18 +19,38 @@ app.get('/', (req: Request, res: Response) => {
 });
 app.post('/api/login',jsonParser,(req: Request<{email:string}>, res: Response) => {
     console.log(req.body)
-    const email = req.body.email
-    const token = jwt.sign({email},secretToken)
+    const email = req.body.email;
+    const token = jwt.sign({email},secretToken);
     res.send(token);
 })
 
 app.post('/api/justify',jsonParser,(req: Request<{token:string,text:string}>, res: Response) => {
-   const decoded = jwt.verify(req.body.token,secretToken)
+   const decoded = jwt.verify(req.body.token,secretToken);
     if(decoded){
-        const justified = justify(req.body.text,80)
+        const words:String[] = req.body.text.split(' ');
+        if(!checkMaxWords(words)){
+            res.send('Payment Required.').status(402);
+            return;
+        }
+        const justified = justify(req.body.text,80);
+        console.log(justified);
+        console.log(maxWordsPerDay);
         res.send(justified);
     }
 })
+
+const checkMaxWords = (words:String[]) =>{
+    let today = new Date();
+    if(today !== currentDay){
+        maxWordsPerDay = 8000;
+        currentDay = today;
+    }
+    if(maxWordsPerDay>words.length){
+        maxWordsPerDay = maxWordsPerDay - words.length;
+        return true
+    }
+    return false
+}
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
--- justify.ts
+++ justify.ts
@@ -2,19 +2,51 @@ const justify = (text:string,desiredLength:number) => {
     let lines = [];
     let index = 0;
     let words = text.split(' ');
-    while (index<words.length){
-
+    while(index < words.length) {
         let count = words[index].length;
         let last = index + 1;
-        if(words[last].length + count + 1 > desiredLength) break;
 
-        // otherwise increase the amount of chars, and go   // to the next word
-        count += words[last].length + 1;
-        last++;
-        index++;
+        while(last < words.length) {
+            if (words[last].length + count + 1 > desiredLength) break;
+            count += words[last].length + 1;
+            last++;
+        }
+
+        let line = "";
+        let difference = last - index - 1;
+
+        // if we're on the last line or the number of words in the line is
+        // 1, we left justify
+        if (last === words.length || difference === 0) {
+            for (let i = index; i < last; i++) {
+                line += words[i] + " ";
+            }
+
+            line = line.substring (0, line.length - 1);
+            for (let i = line.length; i < desiredLength; i++) {
+                line += " ";
+            }
+        } else {
+            // now we need to middle justify, which is putting equal amount
+            // of spaces between words
+            let spaces = (desiredLength - count) / difference;
+            let remainder = (desiredLength - count) % difference;
+
+            for (let i = index; i < last; i++) {
+                line += words[i];
+
+                if( i < last - 1) {
+                    let limit = spaces + ((i - index) < remainder ? 1 : 0)
+                    for (let j = 0; j <= limit; j++) {
+                        line += " ";
+                    }
+                }
+            }
+        }
+        lines.push(line);
+        index = last;
     }
-
-    return text;
+    return lines.join()
 }
 
 export default justify
